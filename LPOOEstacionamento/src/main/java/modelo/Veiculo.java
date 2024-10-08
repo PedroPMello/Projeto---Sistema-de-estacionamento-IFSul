3fde02adc2a3e335d99f3dc367fb9ef467ac2488
PedroPMello <pedro.pmello@outlook.com>
2024-10-07 22:18:47 -0300

    Primeiro commit do projeto

---
+++ LPOOEstacionamento/src/main/java/modelo/Veiculo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+import java.util.List;
+
+/**
+ *
+ * @author pedro
+ */
+public class Veiculo {
+    private String placa;
+    private String cor;
+    private int id;
+    private TipoVeiculo tipoVeiculo;
+    
+    private List<EntradaSaida> listaMovimentacoes;
+}
---
+++ LPOOEstacionamento/src/main/java/modelo/VinculoPessoa.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author pedro
+ */
+public class VinculoPessoa {
+    int servidor;
+    int aluno;
+    int terceirizado;
+    int visitante;
+}
